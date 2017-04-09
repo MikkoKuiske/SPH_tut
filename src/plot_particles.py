b19ba8536283e52657a6cbde16705a61ffdc74e2
VeikkoHaro <veikko.haro@gmail.com>
2017-04-09 15:58:55 +0300

    Python script for creating images

--- src/plot_particles.py
+++ src/plot_particles.py
@@ -3,14 +3,14 @@
 import matplotlib.pyplot as plt
 
 def readfile(name):
-    x_wall, y_wall, x_liquid, y_liquid = [], [], [], []
+    x_wall, y_wall, x_water, y_water = [], [], [], []
     with open(name) as file:
         for line in file:
             row = line.split()
             if row[0] == 'type':
                   if row[1] == 'wall':
                         type = 1
-                  elif row[1] == 'liquid':
+                  elif row[1] == 'water':
                         type = 2
             else:
                 row[0] = int(row[0])
@@ -19,29 +19,29 @@ def readfile(name):
                     x_wall.append(row[0])
                     y_wall.append(row[1])
                 if type == 2:
-                    x_liquid.append(row[0])
-                    y_liquid.append(row[1])
+                    x_water.append(row[0])
+                    y_water.append(row[1])
     file.close()
-    return x_wall, y_wall, x_liquid, y_liquid
+    return x_wall, y_wall, x_water, y_water
 
 def main():
-    filename = "data.txt" #in the end, will be for-loop that reads all relevant files
-    x_wall, y_wall, x_liquid, y_liquid = [], [], [], []
-    x_wall, y_wall, x_liquid, y_liquid = readfile(filename)
-    print(x_wall)
+    filename = "datafile0001.txt" #in the end, will be for-loop that reads all relevant files
+    x_wall, y_wall, x_water, y_water = [], [], [], []
+    x_wall, y_wall, x_water, y_water = readfile(filename)
     
     #border values for plot
-    xmin = min(x_wall+x_liquid)
-    xmax = max(x_wall+x_liquid)
-    ymin = min(y_wall+y_liquid)
-    ymax = max(y_wall+y_liquid)
+    xmin = min(x_wall+x_water)
+    xmax = max(x_wall+x_water)
+    ymin = min(y_wall+y_water)
+    ymax = max(y_wall+y_water)
     
     #draw plot
     for i in range(len(x_wall)):
         plt.plot(x_wall[i],y_wall[i],'go')
-    for i in range(len(x_liquid)):
-        plt.plot(x_liquid[i],y_liquid[i],'bo')
+    for i in range(len(x_water)):
+        plt.plot(x_water[i],y_water[i],'bo')
     plt.axis([xmin,xmax,ymin,ymax])
+    plt.savefig("kuva.png")
     plt.show()
     
 main()
